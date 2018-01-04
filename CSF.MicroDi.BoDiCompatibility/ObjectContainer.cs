267283c11ee6a1fa47bd56715aecfcf576ba9942
Craig Fowler <craig@csf-dev.com>
2018-01-04 22:19:48 +0000

    WIP #7 - Expose the inner container internally in BoDi ObjectContainer

--- CSF.MicroDi.BoDiCompatibility/ObjectContainer.cs
+++ CSF.MicroDi.BoDiCompatibility/ObjectContainer.cs
@@ -34,6 +34,8 @@ namespace BoDi
 
     public event Action<object> ObjectCreated;
 
+    internal IContainer GetMicroDiContainer() => container;
+
     public void RegisterTypeAs<TInterface>(Type implementationType, string name = null) where TInterface : class
     {
       RegisterTypeAs(implementationType, typeof(TInterface), name);
