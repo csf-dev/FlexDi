38aec08a8520ec7fbd122274b2ffa2bf28ee10a4
Craig Fowler <craig@csf-dev.com>
2018-01-05 19:33:47 +0000

    Fix issues with the install and test scripts

--- tools/install.sh
+++ tools/install.sh
@@ -5,4 +5,3 @@ cd "$(dirname "$0")/.."
 nuget restore CSF.MicroDi.sln
 nuget install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory testrunner
 nuget install NUnit.Extension.NUnitV2Driver -Version 3.6.0 -OutputDirectory testrunner
-cp testrunner/NUnit.Extension.NUnitV2Driver.3.6.0/tools/* testrunner/NUnit.ConsoleRunner.3.6.1/tools/
