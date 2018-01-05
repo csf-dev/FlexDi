e90f6c7a3ef3087ccee84fd7f4c9a0f026536e68
Craig Fowler <craig@csf-dev.com>
2018-01-05 21:55:14 +0000

    Unrelated - Improve build & test scripts
    Improved behaviour when something fails

--- tools/build.sh
+++ tools/build.sh
@@ -2,6 +2,19 @@
 
 cd "$(dirname "$0")/.."
 
+function exit_if_failed()
+{
+  if [ "$1" -ne "0" ]
+  then
+    echo "The build step '$2' failed"
+    exit 1
+  fi
+}
+
 ./tools/install.sh
+
 msbuild
+exit_if_failed $? "msbuild"
+
 ./tools/run-tests.sh
+exit_if_failed $? "Unit & integration tests"
