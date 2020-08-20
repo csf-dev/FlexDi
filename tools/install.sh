9b2d6d582753ac59e7b352b71e700e1da701dacb
Craig Fowler <craig@csf-dev.com>
2020-08-20 20:07:08 +0100

    WIP #7 - Update and simplify the build process

--- tools/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.."
-
-nuget restore CSF.FlexDi.sln
-nuget install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory testrunner
-nuget install NUnit.Extension.NUnitV2Driver -Version 3.6.0 -OutputDirectory testrunner
